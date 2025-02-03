ef2e338b6a7e08ae01f495969e607be0974b3dbc
Faldiansyah <faldiansyahk@gmail.com>
2025-02-03 21:00:17 +0700

    selector type

---
+++ selector.js
@@ -0,0 +1,13 @@
+const btn1 = document.getElementById('btn1');
+const btn2 = document.querySelector('.btn2');
+
+console.log(btn1);
+console.log(btn2);
+
+// styling
+btn1.style.backgroundColor = 'red';
+btn1.style.color = 'white';
+btn1.style.border = 'none';
+btn1.style.cursor = 'pointer';
+btn1.style.padding = '10px 20px';
+btn1.style.fontSize = '24px';
