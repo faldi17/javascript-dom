64d5cf3d611a1ba783c3e545c1278fccaeb16b63
Faldiansyah <faldiansyahk@gmail.com>
2025-02-03 20:46:52 +0700

    three ways to input text

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 document.title = 'Belajar JavaScript DOM';
 
 const body = document.body;
-body.append('HELLO WORLD');
-
+/*
+    - jadi textContext dan innerText hampir sama dia hanya masukin teks gak peduli itu tag html atau bukan
+    - sedangkan innerHTML masukin teks dan mendeteksi tag html
+*/
 const h1 = document.createElement('h1'); // <h1></h1>
-h1.textContent = 'Keren Bro Belajar DOM'; // <h1>Keren Bro Belajar DOM</h1>
+h1.textContent = '<marquee>Keren Bro Belajar DOM</marquee>'; // <h1>Keren Bro Belajar DOM</h1>
+
+const myName = document.createElement('p');
+myName.innerHTML = '<marquee>Nama saya Al Khawarizmi</marquee>';
+
+const yourName = document.createElement('b');
+yourName.innerText = '<marquee>Orang yang kusayangi adalah ...</marquee>';
 
 body.append(h1);
+body.append(myName);
+body.append(yourName);
